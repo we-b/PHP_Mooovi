b595a9436ee105d8d8c95b26dd27d74abe3f9f61
Daiki Rokuyama <dr19950228@gmail.com>
2018-08-14 16:56:40 +0900

    get 20 movie products sorted by ascending

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,24 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductsController extends Controller
 {
-  public function index()
-  {
-    $products = array();
-    return view('products.index')->with('products', $products);
-  }
+    public function index()
+    {
+        $products = Product::orderBy('id', 'ASC')->take(20)->get();
+        return view('products.index')->with('products', $products);
+    }
 
-  public function show()
-  {
-    $product = array();
-    return view('products.show')->with('product', product);
-  }
+    public function show()
+    {
+        $product = array();
+        return view('products.show')->with('product', product);
+    }
 
-  public function search()
-  {
-    $products = array();
-    return view('products.search')->with('products', $products);
-  }
+    public function search()
+    {
+        $products = array();
+        return view('products.search')->with('products', $products);
+    }
 }

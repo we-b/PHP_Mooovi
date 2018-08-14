4d5358f664a85bbda3f771231bf3532544682f12
Daiki Rokuyama <dr19950228@gmail.com>
2018-08-14 15:13:08 +0900

    create model, command, migration

---
+++ app/Scraping.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class Scraping
+{
+    public static function movie_urls()
+    {
+        echo "get movies link URL\n";
+    }
+
+    public static function get_product($link)
+    {
+        echo "get movie information\n";
+    }
+}

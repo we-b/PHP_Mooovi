d411888430a17f9450c33d045112b51eee142116
Daiki Rokuyama <dr19950228@gmail.com>
2018-08-16 18:01:25 +0900

    add lines into donw method

--- database/migrations/2018_08_16_080014_remove_nickname_from_reviews_table.php
+++ database/migrations/2018_08_16_080014_remove_nickname_from_reviews_table.php
@@ -26,7 +26,7 @@ class RemoveNicknameFromReviewsTable extends Migration
     public function down()
     {
         Schema::table('reviews', function (Blueprint $table) {
-            //
+            $table->string('nickname');
         });
     }
 }
--- database/migrations/2018_08_16_080148_add_user_id_to_reviews_table.php
+++ database/migrations/2018_08_16_080148_add_user_id_to_reviews_table.php
@@ -30,7 +30,8 @@ class AddUserIdToReviewsTable extends Migration
     public function down()
     {
         Schema::table('reviews', function (Blueprint $table) {
-            //
+            $table->dropForeign('reviews_user_id_foreign');
+            $table->dropColumn('user_id');
         });
     }
 }

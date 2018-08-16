9bb5ce76856d933a806f3d42be69fb16fa7c2ee8
Daiki Rokuyama <dr19950228@gmail.com>
2018-08-16 10:26:13 +0900

    add logic in RankingController

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -6,13 +6,18 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Product;
+use App\Review;
 use View;
+use DB;
 
 class RankingController extends Controller
 {
     public function __construct()
     {
-        $productRanks = Product::take(5)->get();
+        $reviewSort = Review::select(DB::raw('count(*) as num, product_id'))->groupBy('product_id')->orderBy('num', 'DESC')->take(5)->get();
+        $productRanks = $reviewSort->map(function ($review) {
+            return Product::find($review->product_id);
+        });
         View::share('ranking', $productRanks);
     }
 }

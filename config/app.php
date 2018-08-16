7e42e9a710a9629f4b385f67ca3102d2320fbaa8
Daiki Rokuyama <dr19950228@gmail.com>
2018-08-16 16:46:00 +0900

    remove line because of intervention/image

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
          * Package Service Providers...
          */
         Collective\Html\HtmlServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -229,7 +228,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
